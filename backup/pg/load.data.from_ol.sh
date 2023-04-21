3628ebf35f6c4ef9de0f6b0ff3e58116e062886a
wacdev <i@wac.tax>
2023-04-22 06:35:51 +0700

    ♨

--- backup/pg/load.data.from_ol.sh
+++ backup/pg/load.data.from_ol.sh
@@ -6,18 +6,15 @@ set -ex
 
 source .env
 
-if [[ $NN_URI != *_dev* ]]; then
-  echo "数据库名称不包含 _dev 不执行，小心误操作"
+if [[ $PG_URI != *-dev* ]]; then
+  echo "数据库名称不包含 -dev 不执行，小心误操作"
   exit 0
 fi
 
 load_schema() {
   psql postgres://$1 -c "DROP SCHEMA $2 CASCADE" || true
-  # psql postgres://$1 <./dump/xxai.art/ol/table/$2.sql
-  # psql postgres://$1 <./data/ol/$2.sql
-
-  #zstd -qcd
-  # pg_restore postgres://$1 -d $1 ./data/ol/$1.tar
+  psql postgres://$1 <./dump/xxai.art/ol/table/$2.sql
+  zstd -qcd ./data/ol/$2.zstd | pg_restore -d "postgres://$1"
 }
 
 load() {
@@ -26,4 +23,3 @@ load() {
 }
 
 load $PG_URI
-# load $NN_URI
