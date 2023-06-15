eb3815f38e30bda346c4f4d45dcdaceec8c0c45d
wacdev <i@wac.tax>
2023-06-15 17:51:55 +0800

    ♨

--- backup/pg/load.data.from_ol.sh
+++ backup/pg/load.data.from_ol.sh
@@ -4,22 +4,26 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
-source .env
+set -o allexport
+source ./.env
+set +o allexport
 
-if [[ $PG_URI != *-dev* ]]; then
-  echo "数据库名称不包含 -dev 不执行，小心误操作"
-  exit 0
-fi
+./load.coffee
 
-load_schema() {
-  psql postgres://$1 -c "DROP SCHEMA $2 CASCADE" || true
-  psql postgres://$1 <./dump/art-ol/art-ol/table/$2.sql
-  zstd -qcd ./data/art-ol/$2.zstd | pg_restore --disable-triggers -d "postgres://$1"
-}
-
-load() {
-  load_schema $1 bot
-  load_schema $1 img
-}
-
-load $PG_URI
+# if [[ $PG_URI != *-dev* ]]; then
+#   echo "数据库名称不包含 -dev 不执行，小心误操作"
+#   exit 0
+# fi
+#
+# load_schema() {
+#   psql postgres://$1 -c "DROP SCHEMA $2 CASCADE" || true
+#   psql postgres://$1 <./dump/art-ol/art-ol/table/$2.sql
+#   zstd -qcd ./data/art-ol/$2.zstd | pg_restore --disable-triggers -d "postgres://$1"
+# }
+#
+# load() {
+#   load_schema $1 bot
+#   load_schema $1 img
+# }
+#
+# load $PG_URI
